d232d98ea603932413434037558e151d9bc5b20a
Jon Borgonia <jon@gomagames.com>
2015-08-31 18:13:11 -1000

    made the first test requirement pass
    adds myVar variable as a string

--- basics.js
+++ basics.js
@@ -1,4 +1,5 @@
 /* Create a `myName` variable and assign it a String value */
+var myName = 'Jon';
 
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
